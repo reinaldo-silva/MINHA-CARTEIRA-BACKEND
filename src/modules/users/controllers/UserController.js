6d8ff2b910b784dc3109c8fefd5909730c835bda
Reinaldo Silva <reinaldo.silva.jr@outlook.com>
2020-09-11 22:15:27 -0300

    chore: lint-staged

--- src/modules/users/controllers/UserController.js
+++ src/modules/users/controllers/UserController.js
@@ -2,16 +2,16 @@ const SingUpService = require('../services/SingUpServices');
 
 class UserController {
   async create(req, res) {
-    const { name, email, password, password_confirm } = req.body;
+    const { name, email, password, passwordConfirm } = req.body;
 
     if (!name) return res.json({ message: 'name is required' });
     if (!email) return res.json({ message: 'email is required' });
 
     if (!password) return res.json({ message: 'password is required' });
-    if (!password_confirm)
+    if (!passwordConfirm)
       return res.json({ message: 'password_confirm is required' });
 
-    if (password !== password_confirm)
+    if (password !== passwordConfirm)
       return res.json({ message: 'password not match' });
 
     const user = await SingUpService.execute({
